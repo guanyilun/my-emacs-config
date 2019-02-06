43fc7fa3c8dbd610c090c006a614d7fc3926ff5a
Yilun Guan <zoom.aaron@gmail.com>
2019-02-06 18:42:21 -0500

    Add a bunch of parathesis navigating functions

--- lisp-config.el
+++ lisp-config.el
@@ -11,3 +11,58 @@
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 (add-hook 'hy-mode-hook               #'enable-paredit-mode)
+
+;; taken from riktor
+;; https://qiita.com/riktor/items/3389a790f9bd604d211c
+(require 'smartparens)
+(require 'paredit)
+(define-key paredit-mode-map "\C-t" 'transpose-sexps)
+(define-key paredit-mode-map "\M-t" 'reverse-transpose-sexps)
+(define-key paredit-mode-map "\C-k" 'kill-my-sexp)
+(define-key paredit-mode-map "\M-k" 'paredit-kill)
+(define-key paredit-mode-map "\M-f" 'sp-next-sexp)
+(define-key paredit-mode-map "\M-b" 'sp-backward-sexp)
+(define-key paredit-mode-map "\C-h" 'sp-down-sexp)
+(define-key paredit-mode-map "\C-u" 'sp-up-sexp)
+(define-key paredit-mode-map "\C-w" 'sp-copy-sexp)
+(define-key paredit-mode-map (kbd "C-S-w") 'kill-region)
+(define-key paredit-mode-map (kbd "C-,") 'sp-clone-sexp)
+(define-key paredit-mode-map "\M-d" 'paredit-forward-down)
+(define-key paredit-mode-map "\M-u" 'paredit-forward-up)
+(define-key paredit-mode-map "\M-c" 'paredit-convolute-sexp)
+(define-key paredit-mode-map "\C-o" 'avy-goto-sexp-begin)
+(define-key paredit-mode-map (kbd "C-S-o") 'avy-goto-sexp-end)
+
+(defun reverse-transpose-sexps (arg)
+  (interactive "*p")
+  (transpose-sexps (- arg))
+  (backward-sexp  arg)
+  (forward-sexp 1))
+
+(defun kill-my-sexp ()
+  (interactive "*")
+  (if (zerop (length (buffer-substring (point-at-bol) (point-at-eol))))
+      (kill-line)
+      (kill-sexp)))
+
+(require 'avy)
+(setf avy-background nil)
+
+(defun avy-goto-sexp-begin ()
+  (interactive "*")
+  (let ((avy-all-windows nil))
+    (avy-with avy-goto-char
+      (avy--process
+       (avy--regex-candidates
+        (regexp-quote "("))
+       (avy--style-fn avy-style)))))
+
+(defun avy-goto-sexp-end ()
+  (interactive "*")
+  (let ((avy-all-windows nil))
+    (avy-with avy-goto-char
+      (avy--process
+       (avy--regex-candidates
+        (regexp-quote ")"))
+       (avy--style-fn avy-style)))
+    (forward-char)))
