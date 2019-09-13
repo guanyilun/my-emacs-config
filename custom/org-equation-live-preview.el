7d825d702b67ee1ed5e1cfc7eb8101d53599afeb
Yilun Guan <zoom.aaron@gmail.com>
2019-09-13 09:06:30 -0400

    Bug fix

--- custom/org-equation-live-preview.el
+++ custom/org-equation-live-preview.el
@@ -1,19 +1,19 @@
 
-(defvar org-equation-live-preview--timer nil)
 (defvar org-equation-live-preview--buffer-name "*Equation Live*")
-(defvar org-equation-live-preview--org-buffer nil)
-(defvar org-equation-live-preview--preview-buffer nil)
+(defvar org-equation-live-preview--timer)
+(defvar org-equation-live-preview--org-buffer)
+(defvar org-equation-live-preview--preview-buffer)
 
 (defun org-equation-live-preview--preview ()
   (interactive)
   (let ((datum (org-element-context)))
     (and (memq (org-element-type datum) '(latex-environment latex-fragment))
-	 (let ((beg (org-element-property :begin datum))
-	       (end (org-element-property :end datum)))
-           (copy-region-as-kill beg end)
+	 (let* ((beg (org-element-property :begin datum))
+                (end (org-element-property :end datum))
+                (text (buffer-substring-no-properties beg end)))
            (with-current-buffer org-equation-live-preview--preview-buffer
              (let ((inhibit-read-only t))
-               (erase-buffer) (yank)
+               (erase-buffer) (insert text)
                (org-preview-latex-fragment)))))))
 
 (defun org-equation-live-preview--open-buffer ()
@@ -29,15 +29,14 @@
 (defun org-equation-live-preview--activate ()
   (interactive)
   (org-equation-live-preview--open-buffer)
-  (message "Activating org-equation")
+  (message "Activating org-equation-live-preview")
   (setq org-equation-live-preview--timer
         (run-with-idle-timer 0.5 t 'org-equation-live-preview--preview)))
 
 (defun org-equation-live-preview--deactivate ()
   (interactive)
   (delete-other-windows)
-  (message "Deactivating org-equation")
+  (message "Deactivating org-equation-live-preview")
   (cancel-function-timers 'org-equation-live-preview--preview))
-
 
 (provide 'org-equation-live-preview)
